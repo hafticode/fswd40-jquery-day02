644ef78df849030e2b4158439dfe04d53f852418
Tim Almamedov <tim.almamedov@gmail.com>
2018-05-08 11:02:50 +0200

    basic done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
 
-	$(".card").on("click",function(){
-		$(this).append("<p> You clicked on </p>")
+	$(".card").one("click",function(){
+		if ($(this).children('img#bell').length > 0) {
+			$(this).append("<p> You clicked on Bell </p>")
+		} else if ($(this).children('img#santa').length > 0){
+			$(this).append("<p> You clicked on Santa</p>")
+		} else {
+			$(this).append("<p> You clicked on Box</p>")
+		}
 	})
 
 
+	$(".card2").one("click",function(){
+		if ($(this).children('img#santa').length > 0) {
+			$(this).fadeOut();
+		}
+	})
 })
-
-if ($(this).hasDescendant())
