4887fb0145c15d7e908120a680c56b165eb0e423
Tim Almamedov <tim.almamedov@gmail.com>
2018-05-09 09:15:46 +0200

    Shoppin cart v.2 done(not working fully)

--- Intermediate/script/main.js
+++ Intermediate/script/main.js
@@ -8,29 +8,66 @@ $(document).ready(function(){
     })
 
     shoppingCart = [];
-
-    // function addItem() {
-    //     shoppingCart.push(1)
-    // } 
-
-    // $("#button1").on("click", console.log("dgsdfg"))
     
-    
-    
-    $("#button1").on("click", function(){
-        shoppingCart.push(data[0])
-        // $('#cart').append("<div id=\"cart-item\"></div>")
-        div1 = document.createElement("div");
-        $(div1).addClass("cart-item")
-        $('#cart').append(div1);
-        innerDiv = document.createElement("div");
-        $(innerDiv).addClass("cart-item-img")
-        $(div1).append(innerDiv)
-        img1 = $("<img>");
-        img1.attr("src", data[0].image)
-        $(innerDiv).append(img1)
-        $(div1).append(`<div class=\"cart-item-text\"><p>${data[0].name}</p><p>${data[0].price}</p></div>`)
-    })
+   for (let i = 0; i < $(".addToCartButton").length; i++) {
+       
+       $(`.addToCartButton:eq(${i})`).on("click", function(){
+           if (containsObject(data[i], shoppingCart)) {
+               shoppingCart[i].count += 1
+               shoppingCart[i].sum = shoppingCart[i].price * shoppingCart[i].count
+               totalSum = shoppingCart[0].sum
+               if (typeof shoppingCart[1] != 'undefined') {totalSum += shoppingCart[1].sum}
+               if (typeof shoppingCart[2] != 'undefined') {totalSum += shoppingCart[2].sum}
+               if (typeof shoppingCart[3] != 'undefined')  {totalSum += shoppingCart[3].sum}
+               $(`.cart-item-text:eq(${i}) p:first-child`).html(`${shoppingCart[i].name}(${shoppingCart[i].count})`)
+               $(`.cart-item-text:eq(${i}) p:nth-child(2)`).html(shoppingCart[i].sum)
+               $(`#total`).html(`Total: ${totalSum}`)
+            } else {
+                
 
+               shoppingCart.push(data[i])
+               div1 = document.createElement("div");
+               $(div1).addClass("cart-item")
+               $('#cart').append(div1);
+               innerDiv = document.createElement("div");
+               $(innerDiv).addClass("cart-item-img")
+               $(div1).append(innerDiv)
+               img1 = $("<img>");
+               img1.attr("src", data[i].image)
+               $(innerDiv).append(img1)
+               $(div1).append(`<div class=\"cart-item-text\"><p>${data[i].name}</p><p>${data[i].price}</p></div>`)
+           }
+       })
+       
+   }
+//    for (let i = 0; i < $(".addToCartButton").length; i++) {
+       
+//        $(`.addToCartButton:eq(${i})`).on("click", function(){
+//            shoppingCart.push(data[i])
+//            div1 = document.createElement("div");
+//            $(div1).addClass("cart-item")
+//            $('#cart').append(div1);
+//            innerDiv = document.createElement("div");
+//            $(innerDiv).addClass("cart-item-img")
+//            $(div1).append(innerDiv)
+//            img1 = $("<img>");
+//            img1.attr("src", data[i].image)
+//            $(innerDiv).append(img1)
+//            $(div1).append(`<div class=\"cart-item-text\"><p>${data[i].name}</p><p>${data[i].price}</p></div>`)
+//        })
+       
+//    }
+    
+    function containsObject(obj, list) {
+
+    for (i = 0; i < list.length; i++) {
+        if (list[i] === obj) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 })
+
